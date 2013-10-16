9ee742521d7371a92a46a96f0f01649ed7c57aad
Kevin Teynor <kevinteynor@gmail.com>
2013-10-15 21:51:19 -0600

    Map Editor Improvements
    added visual indication for selected tile in the map editor.
    added validation for tile-textures-- cannot add two textures of the same
    name.
    improved maintainability of the MapEditor's OnGUI function.

--- Code/Project/TeykeToolkit/Assets/Editor/ExtensionMethods.cs
+++ Code/Project/TeykeToolkit/Assets/Editor/ExtensionMethods.cs
@@ -7,9 +7,32 @@ public static class ExtensionMethods
     {
         return new Rect(r.xMin + p.x, r.yMin + p.y, r.width, r.height);
     }
+    public static Rect Expand(this Rect r, float value)
+    {
+        return new Rect(r.xMin - value, r.yMin - value, r.width + value + value, r.height + value + value);
+    }
 
     public static Vector2 Position(this Rect r)
     {
         return new Vector2(r.xMin, r.yMin);
+    }
+
+    public static bool Contains(this string[] strs, string s)
+    {
+        for (int i = 0; i < strs.Length; i++)
+            if (strs[i] == s) return true;
+
+        return false;
+    }
+
+    public static bool MemberOf(this string s, string[] strs)
+    {
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].Equals(s))
+                return true;
+        }
+
+        return false;
     }
 }
--- Code/Project/TeykeToolkit/Assets/Editor/Map/MapEditor.cs
+++ Code/Project/TeykeToolkit/Assets/Editor/Map/MapEditor.cs
@@ -3,28 +3,18 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-//[CustomEditor(typeof(Map))]
-//public class MapEditor : Editor
-//{
-//    private static Quaternion flat_quat = Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
+public class MapTilePreview
+{
+    public static MapTilePreview SelectedTile;
+    public static float PreviewSize = 75;
+    public Texture2D Texture;
+    public Rect PreviewPosition;
 
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
-//    }
-
-//    public void OnSceneGUI()
-//    {
-//        Map map = (Map)target;
-
-//        Handles.color = Color.green;
-
-//        foreach (GameObject t in map.Tiles)
-//        {
-//            Handles.RectangleCap(0, t.transform.position, flat_quat, 0.5f);
-//        }
-//    }
-//}
+    public bool IsSelectedTile
+    {
+        get { return MapTilePreview.SelectedTile == this; }
+    }
+}
 
 public class MapEditor : EditorWindow
 {
@@ -40,57 +30,111 @@ public class MapEditor : EditorWindow
         }
     }
 
-    public static List<Texture2D> Textures;
+    public static List<MapTilePreview> Textures;
 
     [MenuItem("Window/Map Editor")]
     static void Init()
     {
         MapEditor window = EditorWindow.GetWindow<MapEditor>();
-        Textures = new List<Texture2D>();
+        Textures = new List<MapTilePreview>();
     }
     
     void OnGUI()
     {
         // toggle the tiles to editable or not.
-        bool showMapTiles = !MapInstance.TilesEditable;
-        showMapTiles = EditorGUILayout.Toggle("Lock Map Tiles", showMapTiles);
-        if (showMapTiles == MapInstance.TilesEditable)
-            MapInstance.TilesEditable = !showMapTiles;
+        bool lockMapTiles = !MapInstance.TilesEditable;
+        lockMapTiles = EditorGUILayout.Toggle("Lock Map Tiles", lockMapTiles);
+        if (lockMapTiles == MapInstance.TilesEditable)
+            MapInstance.TilesEditable = !lockMapTiles;
 
         EditorGUILayout.BeginVertical();
         // selector for the map textures
         // http://docs.unity3d.com/Documentation/ScriptReference/EditorGUI.DrawPreviewTexture.html
-        int x = 0;
-        int y = 60;
-        foreach (Texture2D tex in Textures)
+        Rect newTexturePos = RealignTexturePreviews(0, 60);
+        for (int i = 0; i < Textures.Count; i++)
         {
-            Rect textureRect = new Rect(x, y, 100, 100);
-            EditorGUI.DrawPreviewTexture(textureRect, tex);
-            ObjectNames.GetDragAndDropTitle(tex);
-
-            Event e = Event.current;
-            if(textureRect.Offset(position.Position()).Contains(e.mousePosition) && 
-                (e.type == EventType.MouseDown || e.type == EventType.MouseDrag))
+            if (Textures[i].IsSelectedTile)
             {
-                DragAndDrop.StartDrag(tex.name);
-                e.Use();
+                EditorGUI.DrawRect(Textures[i].PreviewPosition, new Color(0,1,0));
+                EditorGUI.DrawPreviewTexture(Textures[i].PreviewPosition.Expand(-2), Textures[i].Texture);
             }
+            else EditorGUI.DrawPreviewTexture(Textures[i].PreviewPosition, Textures[i].Texture);
+        }
 
-            x += 100;
-            if ((x + 100) >= this.position.width)
+        // spot to add new textures
+        Object newTexture = null;
+        newTexture = EditorGUI.ObjectField(newTexturePos, newTexture, typeof(Texture2D), false);
+        if (newTexture != null)
+        {
+            AddTexture(newTexture as Texture2D);
+        }
+        
+        // tile selection
+        Event e = Event.current;
+        if (e.type == EventType.MouseDown || e.type == EventType.MouseDrag)
+        {
+            MapTilePreview.SelectedTile = null;
+
+            foreach (MapTilePreview mtp in Textures)
             {
-                y += 100;
-                x = 0;
+                if (mtp.PreviewPosition.Contains(e.mousePosition))
+                {
+                    MapTilePreview.SelectedTile = mtp;
+                    e.Use();
+                }
             }
         }
 
-        Object newTexture = null;
-        newTexture = EditorGUI.ObjectField(new Rect(x, y, 100, 100), newTexture, typeof(Texture2D), false);
-        if (newTexture != null)
+        // make texture preview's draggable (to drag onto map tiles
+        foreach (MapTilePreview t in Textures)
         {
-            Textures.Add(newTexture as Texture2D);
+            //Event e = Event.current;
+            //if(textureRect.Offset(position.Position()).Contains(e.mousePosition) && 
+            //    (e.type == EventType.MouseDown || e.type == EventType.MouseDrag))
+            //{
+            //    //Debug.Log(t.name);
+            //    //DragAndDrop.StartDrag(t.name);
+            //    e.Use();
+            //}
         }
 
         EditorGUILayout.EndVertical();
+    }
+
+    private bool AddTexture(Texture2D t)
+    {
+        // check if the t's name is already taken
+        foreach (MapTilePreview mtp in Textures)
+        {
+            if (mtp.Texture.name.Equals(t.name))
+                return false;
+        }
+
+        Textures.Add(new MapTilePreview() { Texture = t });
+        return true;
+    }
+
+    /// <summary>
+    /// Align the texture preview rectangles to fit the dimensions of the window.
+    /// </summary>
+    /// <param name="xoffset">A default x offset to add to all of the tiles.</param>
+    /// <param name="yoffset">A default y offset to add to all of the tiles.</param>
+    /// <returns>A Rect which holds the position and dimensions right after the last texture tile.</returns>
+    Rect RealignTexturePreviews(float xoffset = 0, float yoffset = 0)
+    {
+        for (int i = 0; i < Textures.Count; i++)
+        {
+            MapTilePreview t = Textures[i];
+            Textures[i].PreviewPosition = new Rect(xoffset, yoffset, MapTilePreview.PreviewSize, MapTilePreview.PreviewSize);
+
+            xoffset += MapTilePreview.PreviewSize;
+            if (xoffset + MapTilePreview.PreviewSize >= position.width)
+            {
+                xoffset = 0;
+                yoffset += MapTilePreview.PreviewSize;
+            }
+        }
+
+        return new Rect(xoffset, yoffset, MapTilePreview.PreviewSize, MapTilePreview.PreviewSize);
     }
 }
